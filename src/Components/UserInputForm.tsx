2dc1da0cb54527fbef62bbe5a1a7e5b0a7c2309a
unknown <seledkovti@gmail.com>
2021-06-26 19:18:45 +0300

    v204  homework add email validation

--- src/Components/UserInputForm.tsx
+++ src/Components/UserInputForm.tsx
@@ -4,23 +4,27 @@ import './UserInputForm.scss';
 
 const UserInput = (props: any) => {
   const [enteredName, setEnteredName] = useState('');
+  const [enteredMail, setEnteredMail] = useState('');
   // const [isValidEnteredName, setIsValidEnteredName] = useState(false);
   const [isTouchedEnteredName, setIsTouchedEnteredName] = useState(false);
+  const [isTouchedEnteredMail, setIsTouchedEnteredMail] = useState(false);
   const [formIsValid, setFormIsValid] = useState(false);
   // const nameInputRef: any = useRef();
-  const isValidEnteredName = enteredName.trim() !== '';
+  const isValidEnteredMail = enteredMail.trim() !== '' && enteredMail.includes('@');
+  const isValidEnteredName = enteredName.trim() !== '' && enteredName.length > 2;
   const isInvalidNameInput = !isValidEnteredName && isTouchedEnteredName;
+  const isInvalidMailInput = !isValidEnteredMail && isTouchedEnteredMail;
 
   useEffect(() => {
-    if (isValidEnteredName) {
+    if (isValidEnteredName && isValidEnteredMail) {
       setFormIsValid(true);
     } else {
       setFormIsValid(false);
     }
-  }, [isValidEnteredName]);
+  }, [isValidEnteredName, isValidEnteredMail]);
 
   let formIsValidAlternative = false;
-  if (isValidEnteredName) {
+  if (isValidEnteredName && isValidEnteredMail) {
     formIsValidAlternative = true;
   }
   const formSubmitHandler = (event: FormEvent) => {
@@ -37,7 +41,9 @@ const UserInput = (props: any) => {
     // setIsValidEnteredName(true);
     // nameInputRef.current.value = ''; // bad practice - dom manipulation, this js, not react better use state for shadow dom activate
     setEnteredName('');
+    setEnteredMail('');
     setIsTouchedEnteredName(false);
+    setIsTouchedEnteredMail(false);
   };
   const nameInputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setEnteredName(event.target.value);
@@ -53,12 +59,13 @@ const UserInput = (props: any) => {
   };
   return (
     <form className='user-input-form' onSubmit={formSubmitHandler}>
-      <div className={isInvalidNameInput ? 'user-input-form__name_error' : 'user-input-form__name'}>
+      <div>
         <label htmlFor='name'>
           Your Name: {enteredName}
           {/* {nameInputRef.current.value} */}
         </label>
         <input
+          className={isInvalidNameInput ? 'user-input-form__name_error' : 'user-input-form__name'}
           id='name'
           type='text'
           onChange={nameInputChangeHandler}
@@ -69,7 +76,24 @@ const UserInput = (props: any) => {
         {isInvalidNameInput && (
           <p className='user-input-form__error-message'> Name must not be empty </p>
         )}
+        <label htmlFor='email'>Your email: {enteredMail}</label>
+        <input
+          className={isInvalidMailInput ? 'user-input-form__name_error' : 'user-input-form__name'}
+          type='mail'
+          id='email'
+          onBlur={() => {
+            setIsTouchedEnteredMail(true);
+          }}
+          onChange={(event) => {
+            setEnteredMail(event.target.value);
+          }}
+          value={enteredMail}
+        />
+        {isInvalidMailInput && (
+          <p className='user-input-form__error-message'> Name must not be empty and need @</p>
+        )}
       </div>
+
       <div className='user-input-form__actions'>
         <button disabled={!formIsValidAlternative} type='submit'>
           submit
