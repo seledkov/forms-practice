747cd278dcbfea12492a3099a314c3443a420d4c
unknown <seledkovti@gmail.com>
2021-07-03 17:17:44 +0300

    add style LoginPage and GuestLoginForm

--- src/Components/Login/GuestLoginForm.tsx
+++ src/Components/Login/GuestLoginForm.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { ChangeEvent, FormEvent } from 'react';
 import './GuestLoginForm.scss';
 import passShowIcon from '../../img/icons/passShowIcon.svg';
-import checkboxBox from '../../img/icons/checkbox/checkboxBox.svg';
+import checkboxBox from '../../img/icons/checkboxBox.svg';
 
 const GuestLoginForm = (props: any) => {
   const [enteredLogin, setEnteredLogin] = useState('');
@@ -55,7 +55,9 @@ const GuestLoginForm = (props: any) => {
   return (
     <form className='user-input-form' onSubmit={formSubmitHandler}>
       <div className='user-input-form__wrapper'>
-        <label htmlFor='name'>Логин</label>
+        <label htmlFor='name' className='user-input-form__label'>
+          Логин
+        </label>
         <input
           className={
             isInvalidLoginInput
@@ -73,7 +75,9 @@ const GuestLoginForm = (props: any) => {
         {isError && <p className='user-input-form__error-message'> Неверный логин или пароль</p>}
       </div>
       <div className='user-input-form__wrapper'>
-        <label htmlFor='password'>Пароль</label>
+        <label htmlFor='password' className='user-input-form__label'>
+          Пароль
+        </label>
         <input
           className={
             isInvalidPasswordInput
@@ -94,36 +98,34 @@ const GuestLoginForm = (props: any) => {
           onClick={passwordShowToogle}
         />
       </div>
-      <div>
+      <div className='user-input-form__wrapper flex-sb'>
         <div>
           <label
             htmlFor='login-checkbox'
             className='user-input-form__custom-checkbox custom-checkbox'>
-            {/* todo: create custom checkbox component */}
+            {/* todo: create custom checkbox component and import svg*/}
             <input type='checkbox' id='login-checkbox' className='custom-checkbox__input' />
-            <svg width='32' height='32' viewBox='-4 -4 39 39' aria-hidden='true' focusable='false'>
+            <svg width='20' height='20' viewBox='0 0 20 20' aria-hidden='true' focusable='false'>
               <rect
                 className='checkbox__bg'
-                width='35'
-                height='35'
-                x='-2'
-                y='-2'
+                width='20'
+                height='20'
                 stroke='currentColor'
                 fill='none'
-                strokeWidth='3'
-                rx='6'
-                ry='6'></rect>
-              <polyline
+                rx='6'></rect>
+              <path
                 className='checkbox__checkmark'
-                points='4,14 12,23 28,5'
-                stroke='transparent'
-                strokeWidth='4'
-                fill='none'></polyline>
+                d='M12.9121 7.29886C13.1992 7.01171 13.6648 7.01171 13.952 7.29886C14.2391 7.58601 14.2391 8.05157 13.952 8.33872L9.54019 12.7505C9.25304 13.0376 8.78748 13.0376 8.50033 12.7505L6.04935 10.2995C5.7622 10.0124 5.7622 9.54679 6.04935 9.25964C6.3365 8.97249 6.80206 8.97249 7.08921 9.25964L9.02026 11.1907L12.9121 7.29886Z'
+                stroke='none'
+                fill='transparent'
+              />
             </svg>
             <span className='custom-checkbox__text'>Запомнить меня</span>
           </label>
         </div>
-        <a href='#'>Забыли пароль? </a>
+        <a href='#' className='user-input-form__link'>
+          Забыли пароль?{' '}
+        </a>
       </div>
       <div className='user-input-form__actions'>
         <button className='user-input-form__actions-button' type='submit'>
--- src/Components/Login/LoginPage.tsx
+++ src/Components/Login/LoginPage.tsx
@@ -8,14 +8,22 @@ const LoginPage = (props: any) => {
       <div className='login-page__wrapper'>
         <div className='login-page__bg-text'>testSYSTEM</div>
         <div className='login-page__content'>
-          <div> На главную</div>
-          <div className='login-page__title'>Добро пожаловать в testsystem</div>
-          <GuestLoginForm onSetIsLogin={props.onSetIsLogin} />
-          <div>
-            Вы можете войти через <a href='#'>ЕСИА</a>{' '}
+          <div className='login-page__return'>
+            <span> На главную</span>
           </div>
-          <div>
-            У Вас еще нет аккаунта? <a href='#'>Зарегистрируйтесь</a>{' '}
+          <div className='login-page__content-wrapper'>
+            <div className='login-page__title'>Добро пожаловать в testsystem</div>
+            <GuestLoginForm onSetIsLogin={props.onSetIsLogin} />
+
+            <div className='login-page__links'>
+              <p>
+                Вы можете войти через <a href='#'>ЕСИА</a>
+              </p>
+              <p>
+                {' '}
+                У Вас еще нет аккаунта? <a href='#'>Зарегистрируйтесь</a>{' '}
+              </p>
+            </div>
           </div>
         </div>
       </div>
