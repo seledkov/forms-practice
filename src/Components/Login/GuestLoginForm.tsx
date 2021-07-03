87b466046ddf242942641063e559595b51c52392
unknown <seledkovti@gmail.com>
2021-07-03 14:08:18 +0300

    add custom-checkbox v1

--- src/Components/Login/GuestLoginForm.tsx
+++ src/Components/Login/GuestLoginForm.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { ChangeEvent, FormEvent } from 'react';
 import './GuestLoginForm.scss';
 import passShowIcon from '../../img/icons/passShowIcon.svg';
+import checkboxBox from '../../img/icons/checkbox/checkboxBox.svg';
 
 const GuestLoginForm = (props: any) => {
   const [enteredLogin, setEnteredLogin] = useState('');
@@ -10,6 +11,7 @@ const GuestLoginForm = (props: any) => {
   const [isTouchedEnteredPassword, setIsTouchedEnteredPassword] = useState(false);
   const [isError, setIsError] = useState(false);
   const [passwordIsShow, setPasswordIsShow] = useState(true);
+  const [isLoginCheckbox, setIsLoginCheckbox] = useState(0);
   //validation
   const isValidEnteredLogin = enteredLogin.length > 4 && enteredLogin.trim() !== '';
   const isValidEnteredPassword = enteredPassword.length > 4 && enteredPassword.trim() !== '';
@@ -94,10 +96,33 @@ const GuestLoginForm = (props: any) => {
       </div>
       <div>
         <div>
-          <label htmlFor='remember-checkbox'> Запомнить меня</label>
-          <input type='checkbox' id='remember-checkbox' className='user-input-form__checkbox' />
+          <label
+            htmlFor='login-checkbox'
+            className='user-input-form__custom-checkbox custom-checkbox'>
+            {/* todo: create custom checkbox component */}
+            <input type='checkbox' id='login-checkbox' className='custom-checkbox__input' />
+            <svg width='32' height='32' viewBox='-4 -4 39 39' aria-hidden='true' focusable='false'>
+              <rect
+                className='checkbox__bg'
+                width='35'
+                height='35'
+                x='-2'
+                y='-2'
+                stroke='currentColor'
+                fill='none'
+                stroke-width='3'
+                rx='6'
+                ry='6'></rect>
+              <polyline
+                className='checkbox__checkmark'
+                points='4,14 12,23 28,5'
+                stroke='transparent'
+                stroke-width='4'
+                fill='none'></polyline>
+            </svg>
+            <span className='custom-checkbox__text'>Запомнить меня</span>
+          </label>
         </div>
-
         <a href='#'>Забыли пароль? </a>
       </div>
       <div className='user-input-form__actions'>
